912a158cc3d74b5cc04acf7d6f91fcae274365af
AndrewKrupenko <andrykrupenko338@gmail.com>
2022-08-06 13:30:48 +0300

    finished NFT card view changes

--- components/NFTCard.js
+++ components/NFTCard.js
@@ -3,7 +3,7 @@ import { useNavigation } from "@react-navigation/native";
 
 import { COLORS, SHADOWS, SIZES, assets } from "../constants";
 import { CircleButton, RectButton } from "./Button";
-import { SubInfo } from "./SubInfo";
+import { EthPrice, NFTTitle, SubInfo } from "./SubInfo";
 
 const NFTCard = ({ data }) => {
   const navigation = useNavigation();
@@ -33,6 +33,31 @@ const NFTCard = ({ data }) => {
       </View>
 
       <SubInfo />
+
+      <View style={{ width: "100%", padding: SIZES.font }}>
+        <NFTTitle
+          title={data.name}
+          subTitle={data.creator}
+          titleSize={SIZES.large}
+          subTitleSize={SIZES.small}
+        />
+
+        <View
+          style={{
+            marginTop: SIZES.font,
+            flexDirection: "row",
+            justifyContent: "space-between",
+            alignItems: "center",
+          }}
+        >
+          <EthPrice price={data.price} />
+          <RectButton
+            minWidth={120}
+            fontSize={SIZES.font}
+            handlePress={() => navigation.navigate("Details", { data })}
+          />
+        </View>
+      </View>
     </View>
   );
 };
--- components/SubInfo.js
+++ components/SubInfo.js
@@ -1,18 +1,48 @@
 import { Image, Text, View } from "react-native";
 import { SIZES, assets, COLORS, SHADOWS, FONTS } from "../constants";
 
-export const NFTTitla = () => {
+export const NFTTitle = ({ title, subTitle, titleSize, subTitleSize }) => {
   return (
     <View>
-      <Text>NFTTitla</Text>
+      <Text
+        style={{
+          fontFamily: FONTS.semiBold,
+          fontSize: titleSize,
+          color: COLORS.primary,
+        }}
+      >
+        {title}
+      </Text>
+      <Text
+        style={{
+          fontFamily: FONTS.regular,
+          fontSize: subTitleSize,
+          color: COLORS.primary,
+        }}
+      >
+        {subTitle}
+      </Text>
     </View>
   );
 };
 
-export const EthPrice = () => {
+export const EthPrice = ({ price }) => {
   return (
-    <View>
-      <Text>EthPrice</Text>
+    <View style={{ flexDirection: "row", alignItems: "center" }}>
+      <Image
+        source={assets.eth}
+        resizeMode="contain"
+        style={{ width: 20, height: 20, marginRight: 2 }}
+      />
+      <Text
+        style={{
+          fontFamily: FONTS.medium,
+          fontSize: SIZES.font,
+          color: COLORS.primary,
+        }}
+      >
+        {price}
+      </Text>
     </View>
   );
 };
